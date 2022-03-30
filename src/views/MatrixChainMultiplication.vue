5ed803f8e09390d9963e33285bd69ea89a690888
DocRia <enjrmk@163.com>
2022-03-30 19:52:36 +0800

    finish matrix-chain-multiplication

--- src/views/MatrixChainMultiplication.vue
+++ src/views/MatrixChainMultiplication.vue
@@ -7,7 +7,7 @@
           type="primary"
           plain
           class="stepButton"
-          @click="denoAutoPlay"
+          @click="matrixChainAutoPlay"
         >
           <el-icon><video-play /></el-icon>
         </el-button>
@@ -19,7 +19,7 @@
           type="primary"
           plain
           class="stepButton"
-          @click="denoPause"
+          @click="matrixChainPause"
         >
           <el-icon><video-pause /></el-icon>
         </el-button>
@@ -31,7 +31,7 @@
           type="primary"
           plain
           class="stepButton"
-          @click="denoNextStep"
+          @click="matrixChainNextStep"
         >
           <el-icon><d-arrow-right /></el-icon>
         </el-button>
@@ -43,7 +43,7 @@
           type="primary"
           plain
           class="stepButton"
-          @click="denoPrevStep"
+          @click="matrixChainPrevStep"
         >
           <el-icon><d-arrow-left /></el-icon>
         </el-button>
@@ -75,7 +75,7 @@
           <el-input-number
             class="top-bottom-margin"
             v-if="!isAddFinished"
-            v-model="dimensionNum"
+            v-model="dimensionInput"
             :min="1"
           />
         </el-row>
@@ -97,13 +97,34 @@
         </el-row>
       </el-col>
 
-      <el-col :span="15" v-if="isAddFinished">
+      <el-col :span="tableSpan" v-if="isAddFinished">
         <el-table
           :data="matrixDP"
+          class="matrixDPTable"
+          stripe
           border
           fit
         >
-          <el-table-column v-for="col in matrixDP" :key="col.title" :label="col.title">
+          <el-table-column label="矩阵链相乘次数">
+            <el-table-column type="index" label="行\列" :width="60">
+            </el-table-column>
+            <el-table-column v-for="col in matrixColumnList" :key="col.prop" :prop="col.prop" :label="col.label">
+            </el-table-column>
+          </el-table-column>
+        </el-table>
+
+        <el-table
+          :data="matrixBreakDP"
+          class="matrixBreakDPTable"
+          stripe
+          border
+          fit
+        >
+          <el-table-column label="矩阵链分割点">
+            <el-table-column type="index" label="行\列" :width="60">
+            </el-table-column>
+            <el-table-column v-for="col in matrixColumnList" :key="col.prop" :prop="col.prop" :label="col.label">
+            </el-table-column>
           </el-table-column>
         </el-table>
       </el-col>
@@ -117,7 +138,9 @@ import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 
 const isAddFinished = ref(false)
-const dimensionNum = ref(1)
+const matrixChainAutoPlaySetting = ref(0)
+const dimensionInput = ref(1)
+const tableSpan = ref(16)
 const matrixData = ref([
   { dimension: 10 },
   { dimension: 100 },
@@ -126,6 +149,14 @@ const matrixData = ref([
 ])
 const matrixDataFull = ref([])
 const matrixDP = ref([])
+const matrixBreakDP = ref([])
+const matrixColumnList = ref([])
+let chainSpan = 1
+let row = 0
+let col = 0
+let template = 0
+let printMessage = ''
+let calFinished = false
 
 const addFinish = () => {
   if (matrixData.value.length < 4) {
@@ -139,44 +170,141 @@ const addFinish = () => {
         minfo: `[${matrixData.value[i - 1].dimension}, ${matrixData.value[i].dimension}]`
       })
     }
-    const firstCol = { title: '行\\列' }
+
     for (let i = 1; i < matrixData.value.length; i++) {
-      Reflect.defineProperty(firstCol, `row${String.fromCharCode(64 + i)}`, {
-        value: i
-      })
-    }
-    matrixDP.value.push(firstCol)
-    for (let i = 1; i < matrixData.value.length; i++) {
-      const col = { title: `${i}` }
+      const col = {}
+      matrixColumnList.value.push({ prop: `${i - 1}`, label: `${i}` })
       for (let j = 1; j < matrixData.value.length; j++) {
-        Reflect.defineProperty(col, `row${String.fromCharCode(64 + j)}`, {
-          value: 0
-        })
+        col[j - 1] = 0
       }
-      matrixDP.value.push(col)
+      matrixDP.value.push({ ...col })
+      matrixBreakDP.value.push({ ...col })
     }
-    console.log(matrixDP.value)
     isAddFinished.value = true
   }
 }
 
 const resetMatrix = () => {
+  if (matrixChainAutoPlaySetting.value !== 0) {
+    return ElMessage.error({
+      message: '请暂停自动演示或等待演示完成'
+    })
+  }
   matrixDataFull.value = []
+  matrixColumnList.value = []
+  matrixDP.value = []
+  matrixBreakDP.value = []
   isAddFinished.value = false
+  calFinished = false
+  chainSpan = 1
+  row = 0
+  col = 0
 }
 
 const addMatrix = () => {
-  dimensionNum.value = Number.parseInt(dimensionNum.value)
-  matrixData.value.push({ dimension: dimensionNum.value })
+  dimensionInput.value = Number.parseInt(dimensionInput.value)
+  matrixData.value.push({ dimension: dimensionInput.value })
 }
 
 const deleteMatrix = (index) => {
   matrixData.value.splice(index, 1)
 }
 
+const printOrder = (i, j) => {
+  if (i === j) {
+    printMessage += ` A[${i}] `
+  } else {
+    printMessage += ' ( '
+    printOrder(i, matrixBreakDP.value[i - 1][j - 1])
+    printOrder(matrixBreakDP.value[i - 1][j - 1] + 1, j)
+    printMessage += ' ) '
+  }
+}
+
+// 步骤函数
+const matrixChainNextStep = () => {
+  if (calFinished) {
+    matrixChainPause()
+    return ElMessage.success({
+      message: '计算完成：' + printMessage
+    })
+  }
+  if (row === matrixData.value.length - chainSpan - 1) {
+    chainSpan++
+    row = 0
+  }
+  if (chainSpan === matrixData.value.length - 1) {
+    calFinished = true
+    matrixChainPause()
+    printMessage = ''
+    printOrder(1, chainSpan)
+    printMessage = printMessage.slice(2, -3)
+    return ElMessage.success({
+      message: '计算完成：' + printMessage
+    })
+  }
+  col = row + chainSpan
+  matrixDP.value[row][col] = Number.MAX_SAFE_INTEGER
+  for (let k = row; k <= col - 1; k++) {
+    template = matrixDP.value[row][k] + matrixDP.value[k + 1][col] + matrixData.value[row].dimension * matrixData.value[k + 1].dimension * matrixData.value[col + 1].dimension
+    if (template < matrixDP.value[row][col]) {
+      matrixDP.value[row][col] = template
+      matrixBreakDP.value[row][col] = k + 1
+    }
+  }
+  let currMessage = ' ( '
+  for (let m = row; m <= col; m++) {
+    currMessage += ` A[${m + 1}] `
+    if (m === matrixBreakDP.value[row][col] - 1) {
+      currMessage += ' )( '
+    }
+  }
+  currMessage += ' ) '
+  ElMessage.success({
+    message: currMessage
+  })
+  row++
+}
+
+const matrixChainPrevStep = () => {
+  if (calFinished) {
+    calFinished = false
+  }
+  if (row === 0) {
+    if (chainSpan === 1) {
+      return ElMessage.error({
+        message: '已经是第一步了'
+      })
+    }
+    row = matrixData.value.length - chainSpan - 1
+    chainSpan--
+  } else {
+    row--
+  }
+  col = row + chainSpan
+  matrixDP.value[row][col] = 0
+  matrixBreakDP.value[row][col] = 0
+}
+
+const matrixChainAutoPlay = () => {
+  if (matrixChainAutoPlaySetting.value !== 0) {
+    return ElMessage.warning({
+      message: '正在自动演示'
+    })
+  }
+  matrixChainAutoPlaySetting.value = setInterval(matrixChainNextStep, 1000)
+}
+
+const matrixChainPause = () => {
+  if (matrixChainAutoPlaySetting.value !== 0) {
+    clearInterval(matrixChainAutoPlaySetting.value)
+    matrixChainAutoPlaySetting.value = 0
+  }
+}
+
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 .flex-line-center {
   display: flex;
   justify-content: center;
@@ -192,4 +320,9 @@ const deleteMatrix = (index) => {
 .top-bottom-margin {
   margin: 20px auto;
 }
+
+.matrixDPTable {
+  margin-bottom: 30px;
+}
+
 </style>
